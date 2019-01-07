abc8b160d362f1ff4fbfa3dcaec0845f29df6ccb
chenkang <1447102970@qq.com>
2019-01-07 22:23:14 +0800

    分类完

--- jiaoshou/vue/src/components/fenlei.vue
+++ jiaoshou/vue/src/components/fenlei.vue
@@ -18,6 +18,16 @@
                  <ul>
                      <li v-for="item in re" :key="item.id">{{item}}</li>
                  </ul>
+                 <p>热门单品</p>
+                 <div class="cdan" v-for="item in xinghao" :key="item.id">
+                     <h2>商品图</h2>
+                     <div class="cxinghao">
+                     <p>型号</p>
+                     <p>{{item.name}}</p>
+                     <p>价格</p>
+                     <p>{{item.age}}</p>
+                     </div>
+                 </div>
              </div>
          </div>
      </section>
@@ -31,7 +41,8 @@ export default {
     name:'fenlei',
     data() {
         return {
-            re:['挂壁式空调','挂壁式空调','挂壁式空调','挂壁式空调','挂壁式空调']
+            re:['挂壁式空调','挂壁式空调','挂壁式空调','挂壁式空调','挂壁式空调'],
+            xinghao:[{name:'t',age:'3'},{name:'t',age:'3'},{name:'t',age:'3'}]
         }
     },
     methods: {
@@ -69,15 +80,37 @@ header span{
     margin: 10px;
 }
 section{
+    overflow-y: auto;
     flex: 1;
 }
 .cleft{
+    position: fixed;
     width: 25%;
     height: 100%;
     background: #eaeaea;
 }
 .cright{
+    margin-left: 25%;
     width: 75%
    
+}
+.cright ul{
+    display: flex;
+    flex-wrap: wrap;
+}
+.cright li{
+    width: 32.5%;
+    height:92px ;
+    border:1px solid #e2e2e2;
+}
+.cdan{
+   display: flex;
+
+}
+.cdan h2{
+    width:30%;
+}
+.cxinghao{
+    width: 70%;
 }
 </style>
